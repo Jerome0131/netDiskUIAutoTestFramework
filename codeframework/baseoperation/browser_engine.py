6459e4be1076ce260bd4817872c5d6eb12102c39
Jerome <wangchen8264957@163.com>
2018-04-09 11:44:44 +0800

    1.规范一些格式问题

--- codeframework/baseoperation/browser_engine.py
+++ codeframework/baseoperation/browser_engine.py
@@ -12,7 +12,8 @@ logger = Logger("BrowserEngine")
 class BrowserEngine(object):
 
     def __init__(self):
-        # 读取配置文件
+        """
+        # 读取配置文件config.ini
         # config = configparser.ConfigParser()
         # configfile_path = os.path.join(os.path.abspath(os.path.join(os.path.abspath('.'), '../..')), 'resources/config/config.ini')
         # config.read(configfile_path, 'utf-8')
@@ -20,7 +21,7 @@ class BrowserEngine(object):
         # 获取浏览器类型
         # browser = config.get("browserType", "browser")
         # logger.info("选择的浏览器是%s." % browser)
-
+        """
         browser = config.BROWSER
 
         if browser == "Chrome":
