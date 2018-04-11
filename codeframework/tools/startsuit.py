9fc31d3c44d1fcaa37e30dfc1768fa3408cc1e8f
Jerome <wangchen8264957@163.com>
2018-04-11 16:26:44 +0800

    1.规范一些代码格式，去掉无效的import包 2.增加startsuit工具类，每个测试套件的执行之前都需要使用此类

---
+++ codeframework/tools/startsuit.py
@@ -0,0 +1,44 @@
+# coding:utf-8
+import time
+
+import os
+
+from codeframework.baseoperation.base_page import BasePage
+from codeframework.baseoperation.browser_engine import BrowserEngine
+from codeframework.tools.html_test_runner import HTMLTestRunner
+from resources.config import config
+
+
+class StartSuit(object):
+    """
+    启动类，主要功能：
+    1.唯一的创建driver入口
+    2.加载所有的casesuit，并生成html的测试报告
+    3.退出浏览器的方法，作为测试结束
+
+    testsuits包中所有的测试套件，都需要使用此工具类
+    """
+    @staticmethod
+    def init_driver():
+        # 唯一创建driver入口
+        BrowserEngine.get_instance().driver = BrowserEngine.get_driver()
+        BasePage(BrowserEngine.get_instance().driver).open_url()
+
+    @staticmethod
+    def init_report_run(casesuit):
+        # 取前面时间加入到测试报告文件名中
+        now = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
+
+        # 如果report目录不存在，则创建一个
+        if not os.path.exists(config.REPORT_PATH):
+            os.makedirs(config.REPORT_PATH)
+
+        filename = os.path.join(config.REPORT_PATH, "result" + now + ".html")
+        fp = open(filename, 'wb')
+        runner = HTMLTestRunner(stream=fp, title='Report_title', description='Report_description')
+        runner.run(casesuit)
+        fp.close()
+
+    @staticmethod
+    def quit_browser():
+        BasePage(BrowserEngine.get_instance().driver).quit_browser()
